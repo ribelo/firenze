c82934bbe674d12b8e437ccf4fef2bc239044dd2
ribelo <r.krzywaznia@gmail.com>
2020-05-15 23:13:17 +0200

    Changes to be committed: 	modified:   deps.edn 	deleted:    src/ribelo/firenze.cljs 	deleted:    src/ribelo/firenze/datascript.cljs 	deleted:    src/ribelo/firenze/firebase.cljs 	deleted:    src/ribelo/firenze/firestore.cljs 	deleted:    src/ribelo/firenze/realtime_database.cljs 	deleted:    src/ribelo/firenze/utils.cljs

--- src/ribelo/firenze/firebase.cljs
+++
@@ -1,9 +0,0 @@
-(ns ribelo.firenze.firebase
-  (:require
-   ["firebase/app" :as firebase]
-   [applied-science.js-interop :as j]
-   [cljs-bean.core :as bean :refer [->js]]))
-
-
-(defn initialize-app [firebase-app-info]
-  (j/call firebase :initializeApp (->js firebase-app-info)))
